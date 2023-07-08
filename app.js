7e1e7fc49fd646c9fc2d9e85075c9b2481f8a8d1
Awwal <awwallawal@gmail.com>
2023-07-08 13:26:55 +0100

    Updates

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ let email = document.querySelector("#email");
 let message = document.querySelector("#message");
 
 formContainer.addEventListener('submit', function(e){
- e.preventDefault();
  name.value = "";
  email.value = "";
  message.value = "";
