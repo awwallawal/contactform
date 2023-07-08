8cc6cec42a81c52018039f3e4bdfdd09b2e869fa
Awwal <awwallawal@gmail.com>
2023-07-08 12:45:26 +0100

    Remove the link used within the send button since it was used for local development testing, cleared the input fields with JavaScript to return the form to its original state and modified the css appropriately

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 let formContainer = document.querySelector("#form-container");
+let name = document.querySelector("#name");
+let email = document.querySelector("#email");
+let message = document.querySelector("#message");
 
 formContainer.addEventListener('submit', function(e){
  e.preventDefault();
- 
+ name.value = "";
+ email.value = "";
+ message.value = "";
 })
